891ebdf8201d7b450e17596c6a5d9111804a4920
vatech-younghoon <younghoon.choi@vatech.com>
2021-09-27 10:15:16 +0900

    [feat] add todo query

---
+++ src/queries/todo.js
@@ -0,0 +1,51 @@
+import { gql } from "@apollo/client";
+
+const SEARCH_TODO = gql`
+  query searchTodo(
+    $args: InputSearhTodo
+    $comparison: String
+    $match: String
+    $sort: InputSort
+  ) {
+    searchTodo(
+      args: $args
+      comparison: $comparison
+      match: $match
+      sort: $sort
+    ) {
+      id
+      text
+      done
+    }
+  }
+`;
+
+const CREATE_TODO = gql`
+  mutation createTodo($id: String!, $text: String!, $done: Boolean!) {
+    createTodo(id: $id, text: $text, done: $done) {
+      id
+      text
+      done
+    }
+  }
+`;
+
+const REMOVE_TODO = gql`
+  mutation removeTodo($id: String!) {
+    removeTodo(id: $id) {
+      id
+    }
+  }
+`;
+
+const UPDATE_TODO = gql`
+    mutation updateTodo($id: String!, $text:String! done:Boolean!) {
+        updateTodo(id: $id, text: $text, done:$done) {
+            id
+            text
+            done
+        }
+    }
+`;
+
+export { SEARCH_TODO, CREATE_TODO, REMOVE_TODO, UPDATE_TODO };
